4afe3ddd49555f9b30fd542cf2323c96faa230c4
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-10-09 23:18:31 +0700

    display mesage using toast

--- app/src/main/java/com/bhtu/diceroller/MainActivity.kt
+++ app/src/main/java/com/bhtu/diceroller/MainActivity.kt
@@ -2,10 +2,20 @@ package com.bhtu.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button2);
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice rolled", Toast.LENGTH_LONG);
+            toast.show()
+        }
+
+
     }
 }
